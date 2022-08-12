33497db290822ff5f7f11b0debb6c5cc25baa66c
danbara01703 <danbara01703@stnet.co.jp>
2022-08-12 13:00:06 +0900

    魔剤追加モーダルを作成

--- mazain/src/app/domain/presentations/share/mazain-card-plus/mazain-card-plus.component.ts
+++ mazain/src/app/domain/presentations/share/mazain-card-plus/mazain-card-plus.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'mazain-card-plus',
@@ -6,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mazain-card-plus.component.scss'],
 })
 export class MazainCardPlusComponent implements OnInit {
+  @ViewChild(IonModal) modal: IonModal;
 
-  constructor() { }
+  inputForm = this.formBuilder.group<RegistryForm>({
+    name: new FormControl('', { nonNullable: true }),
+    coffeIn: new FormControl(0, { nonNullable: true }),
+    sugar: new FormControl(0, { nonNullable: true }),
+    kcal: new FormControl(0, { nonNullable: true }),
+    imgData: new FormControl('', { nonNullable: true })
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() { }
 
+
+  async onClickCancelBtn() {
+    await this.modal.dismiss();
+  }
+
+  /**
+   * 登録ボタン押下時
+   */
+  async onClickRegistryBtn() {
+
+  }
+
+}
+
+
+interface RegistryForm {
+  name: FormControl<string>;
+  coffeIn: FormControl<number>;
+  sugar: FormControl<number>;
+  kcal: FormControl<number>;
+  imgData: FormControl<string>;
 }
--- mazain/src/app/domain/presentations/share/mazain-card-plus/mazain-card-plus.module.ts
+++ mazain/src/app/domain/presentations/share/mazain-card-plus/mazain-card-plus.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { MazainCardPlusComponent } from './mazain-card-plus.component';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule,],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+  ],
   declarations: [MazainCardPlusComponent],
   exports: [MazainCardPlusComponent]
 })
-export class MazainCardPlusComponentModule {}
+export class MazainCardPlusComponentModule { }
