2f3f96eaa039584e754a6c7c616a3858323c2496
DJ <georgetech1984@gmail.com>
2022-08-21 18:51:31 +0900

    iosビルドファイル追加

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MazaIn
  魔剤管理アプリ
+
+
+## iosビルド
+プロジェクトフォルダ内で以下のコマンドを実行
+- `ionic build --prod`
+- `ionic integrations enable capacitor`
+- `ionic cap add ios`
+
+Xcodeで開く
+- `npx cap open ios`
---
+++ mazain/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mazain',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
