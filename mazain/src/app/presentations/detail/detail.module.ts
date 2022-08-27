df77b9eb16f6581aa72a5450b9fd838ff9a6a222
DJ <georgetech1984@gmail.com>
2022-08-27 22:37:41 +0900

    コミットもれ

--- mazain/src/app/presentations/detail/detail.module.ts
+++ mazain/src/app/presentations/detail/detail.module.ts
@@ -10,6 +10,8 @@ import { DetailPage } from './detail.page';
 import { HeaderComponentModule } from '../share/header/header.component.module';
 import { MazaiShowcaseComponentModule } from '../share/mazai-showcase/mazai-showcase.component.module';
 import { HelperCardComponentModule } from '../share/helper-card/help-card.component.module';
+import { MazaiEnergyInfoPopoverComponentModule } from '../share/mazai-energy-info-popover/mazai-energy-info-popover.module';
+
 
 @NgModule({
   imports: [
@@ -20,6 +22,7 @@ import { HelperCardComponentModule } from '../share/helper-card/help-card.compon
     HeaderComponentModule,
     MazaiShowcaseComponentModule,
     HelperCardComponentModule,
+    MazaiEnergyInfoPopoverComponentModule,
   ],
   declarations: [DetailPage]
 })
